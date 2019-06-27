2816d93ed8b8cba2744eca421785dd118bc0fdc3
nkalupahana <avishkank@gmail.com>
2019-06-27 14:58:35 -0700

    Forwards functions working

--- neuron.cpp
+++ neuron.cpp
@@ -11,7 +11,7 @@ class Neuron {
     
     public:
         Neuron(int inputs, string act) : activationFunc(act) {
-            assert(("Only 'relu' activation supported", act == "relu"));
+            assert(("Only 'relu' or 'linear' activation supported", act == "relu" || act == "linear"));
             assert(("Inputs to neuron must be greater than 0!", inputs > 0));
 
             bias = 0;
@@ -52,6 +52,8 @@ class Neuron {
                 }
 
                 return ret;
+            } else if (activationFunc == "linear") {
+                return ret;
             }
 
             return -1;
@@ -59,11 +61,10 @@ class Neuron {
 };
 
 class NeuronLayer {
-    int inputs;
     string activationFunc;
     vector<Neuron*> neurons;
     public:
-        NeuronLayer(int in, int num, string act) {
+        NeuronLayer(int in, int num, string act) : activationFunc(act) {
             assert(("Neurons in layer must be greater than 0!", num > 0));
 
             for (int i = 0; i < num; i++) {
@@ -94,23 +95,32 @@ class NeuronLayer {
 
 class NeuralNet {
     vector<NeuronLayer*> layers;
-    void add_layer(int number_of_neurons, int inputs, string activationFunc) {
+    void add_layer(int inputs, int number_of_neurons, string activationFunc) {
         layers.push_back(new NeuronLayer(inputs, number_of_neurons, activationFunc));
     }
+
     public:
-        NeuralNet(int inputs, string activationFunc) {
-            add_layer(inputs, inputs, activationFunc);
+        NeuralNet(int inputs, int neurons, string activationFunc) {
+            add_layer(inputs, neurons, activationFunc);
         }
 
         void add_layer(int number_of_neurons, string activationFunc) {
             add_layer(layers.back()->getNumberOfNeurons(), number_of_neurons, activationFunc);
         }
 
-        float forwards() {
-            //assert(("Only linear final activation supported."), (layers.back()->getActivationFunc() == "linear"));
-            //assert(("Final activation layer only supports one neuron."), layers.back()->getNumberOfNeurons() == 1));
+        float forwards(vector<float> inputs) {
+            assert(("Only linear final activation supported.", layers.back()->getActivationFunc() == "linear"));
+            assert(("Final activation layer only supports one neuron.", layers.back()->getNumberOfNeurons() == 1));
+            assert(("You need at least one layer!", layers.size() > 0));
 
-            
+            vector<NeuronLayer*>::iterator nlit = layers.begin();
+            vector<float> in = inputs;
+            for (int i = 0; i < layers.size(); i++) {
+                in = ((*nlit)->forwards(in));
+                nlit++;
+            }
+
+            return in.front();
         }
 };
 
@@ -128,6 +138,19 @@ int main() {
 
     */
 
-   NeuronLayer* nl = new NeuronLayer(5, 10, "relu");
+   //NeuronLayer* nl = new NeuronLayer(2, 5, "relu");
 
+   NeuralNet* net = new NeuralNet(2, 5, "relu");
+   net->add_layer(10, "relu");
+   net->add_layer(10, "relu");
+   net->add_layer(10, "relu");
+   net->add_layer(1, "linear");
+
+   vector<float> input;
+   input.push_back(4);
+   input.push_back(7);
+
+
+   float res = net->forwards(input);
+   std::cout << res << "\n";
 }
